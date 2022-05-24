c6b39e5e8388a97a0ba13de094e975b44503ee03
GHogbin <graham.hogbin@microsoft.com>
2022-05-24 15:53:45 +0100

    Add files via upload

--- appgwurl.ps1
+++ appgwurl.ps1
@@ -12,3 +12,15 @@ $imagevalue = "Images: " + $($env:computername)
 Add-Content -Path "C:\inetpub\wwwroot\images\test.htm" -Value $imagevalue
 $videovalue = "Video: " + $($env:computername)
 Add-Content -Path "C:\inetpub\wwwroot\video\test.htm" -Value $videovalue
+$filesvalue = "files: " + $($env:computername)
+Add-Content -Path "C:\inetpub\wwwroot\video\test.htm" -Value $filesvalue
+$datavalue = "data: " + $($env:computername)
+Add-Content -Path "C:\inetpub\wwwroot\video\test.htm" -Value $datavalue
+$uploadvalue = "upload: " + $($env:computername)
+Add-Content -Path "C:\inetpub\wwwroot\video\test.htm" -Value $uploadvalue
+$dumpvalue = "dump: " + $($env:computername)
+Add-Content -Path "C:\inetpub\wwwroot\video\test.htm" -Value $dumpvalue
+$outputvalue = "output: " + $($env:computername)
+Add-Content -Path "C:\inetpub\wwwroot\video\test.htm" -Value $outputvalue
+$diagvalue = "diag: " + $($env:computername)
+Add-Content -Path "C:\inetpub\wwwroot\video\test.htm" -Value $diagvalue
